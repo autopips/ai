b4b10c3f77cecf3f8c634fd35ea95c81b6cccb02
autopips <autopipsforex@gmail.com>
2024-07-18 12:09:59 +0200

    Create signUp.js

---
+++ signUp.js
@@ -0,0 +1,27 @@
+function signUpFirebase(){
+    
+      var firebaseConfig = {
+      apiKey: "AIzaSyAKgvn2crUGUCgOZ5S0RT84iS_6ocNn8z8",
+      authDomain: "https://www.google.com",
+      projectId: "autopips-f6220",
+      storageBucket: "YOUR_STORAGE_BUCKET",
+      messagingSenderId: "154809636444",
+      appId: "YOUR_APP_ID"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function(user) {
+          
+          alert("Sign up successful!");
+          window.location.href=("https://www.google.com/");
+          
+        })
+        .catch(function(error) {
+          console.log(error.message);
+        });
+    
+}
