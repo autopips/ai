ec4b4d58e76ffa136f3f1b0205f5cb452d28f92e
autopips <autopipsforex@gmail.com>
2024-07-26 14:12:18 +0200

    Update loginFirebase.js

--- loginFirebase.js
+++ loginFirebase.js
@@ -60,9 +60,17 @@ function signInFirebase(){
 
             
                var refCode = document.getElementById("refCode");
+               var refCount = document.getElementById("refCount");
+               var refBal = document.getElementById("refBal");
+               var bal2 = document.getElementById("bal2");
+            
                refCode.innerHTML=("RefCode: "+email);
                if(email=="gmasele15@gmail.com"){
                    refCode.innerHTML=("Ref19991120");
+                   
+                   bal2.innerHTML=("USD 528.29");
+                   refCount.innerHTML=("28");
+                   refBal.innerHTML=("USD 29.74");
                }
           
             
