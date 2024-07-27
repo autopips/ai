a836e4c5abe00596ff645cc6a687b7d0dd4fc682
autopips <autopipsforex@gmail.com>
2024-07-27 21:43:09 +0200

    Update balances.js

--- balances.js
+++ balances.js
@@ -1,4 +1,35 @@
 
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  
+         var bal22 = document.getElementById("bal2");
+
+
+               var randomNum = Math.floor(Math.random() * (80 - 50 + 1) + 50);
+               var randomNum2 = Math.floor(Math.random() * (10 - 20 + 3) + 30);
+
+
+               bal22.innerHTML = "USD: " + "<span style='color: #000000; font-weight: bold;'> $9" + randomNum +"."+ randomNum2;
+     
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function updateBalance{
 
 
