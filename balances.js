fe2c4159046cd34bba18d656a06c56dceb7fe465
autopips <autopipsforex@gmail.com>
2024-07-27 21:46:17 +0200

    Update balances.js

--- balances.js
+++ balances.js
@@ -3,16 +3,18 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  
+        
+         var email = document.getElementById("email").value;
          var bal22 = document.getElementById("bal2");
 
-
+         if(email=="gmasele15@gmail.com"){
                var randomNum = Math.floor(Math.random() * (80 - 50 + 1) + 50);
                var randomNum2 = Math.floor(Math.random() * (10 - 20 + 3) + 30);
 
 
                bal22.innerHTML = "USD: " + "<span style='color: #000000; font-weight: bold;'> $9" + randomNum +"."+ randomNum2;
-     
+         }
+  
 }); 
 
 
