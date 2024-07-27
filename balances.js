1eef0a2054bf8f2581e02fbe52c68273e505e3b4
autopips <autopipsforex@gmail.com>
2024-07-27 20:40:50 +0200

    Create balances.js

---
+++ balances.js
@@ -0,0 +1,18 @@
+
+function updateBalance{
+
+
+        var email = document.getElementById("email").value;
+
+       if (email=="gmasele15@gmail.com"){
+  
+               var randomNum = Math.floor(Math.random() * (80 - 50 + 1) + 50);
+               var randomNum2 = Math.floor(Math.random() * (10 - 20 + 3) + 30);
+
+               var bal2 = document.getElementById("bal2");
+
+               bal2.innerHTML = "USD: " + "<span style='color: #000000; font-weight: bold; font-size: 17px;'> $9" + randomNum +"."+ randomNum2;
+     
+      }
+
+}
