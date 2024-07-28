d9f9c06bfbfa4312534d2bb55528ecead73ae3e8
autopips <autopipsforex@gmail.com>
2024-07-28 11:52:54 +0200

    Update balances.js

--- balances.js
+++ balances.js
@@ -1,51 +1,34 @@
 
 
-
-
-document.addEventListener("DOMContentLoaded", function() {
+function updateNumber() {
+  
         
-         var email = document.getElementById("email").value;
-         var bal22 = document.getElementById("bal2");
-
-         if(email=="gmasele15@gmail.com"){
-               var randomNum = Math.floor(Math.random() * (80 - 50 + 1) + 50);
-               var randomNum2 = Math.floor(Math.random() * (10 - 20 + 3) + 30);
-
-
-               bal22.innerHTML = "USD: " + "<span style='color: #000000; font-weight: bold;'> $9" + randomNum +"."+ randomNum2;
-         }
-  
-}); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function updateBalance{
-
-
-        var email = document.getElementById("email").value;
-
-       if (email=="gmasele15@gmail.com"){
+     var email = document.getElementById("email").value;
+      
+      
+      if (email=="gmasele15@gmail.com"){
   
                var randomNum = Math.floor(Math.random() * (80 - 50 + 1) + 50);
                var randomNum2 = Math.floor(Math.random() * (10 - 20 + 3) + 30);
 
-               var bal2 = document.getElementById("bal2");
+               var numberElem = document.getElementById("bal2");
 
-               bal2.innerHTML = "USD: " + "<span style='color: #000000; font-weight: bold; font-size: 17px;'> $9" + randomNum +"."+ randomNum2;
+               numberElem.innerHTML = "USD: " + "<span style='font-weight: bold;'> $9" + randomNum +"."+ randomNum2;
      
       }
+     
+   
 
 }
+
+setInterval(updateNumber, 1000);
+
+
+
+
+
+
+
+
+
+
